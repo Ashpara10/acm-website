cc8381e0ba0729c668a91c5e72b8dff219aa1990
Atharv Vani <atharvavani110@gmail.com>
2024-10-11 01:14:36 +0530

    fix: added code to make navbar width same as of footer

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -78,61 +78,67 @@ export const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   const path = usePathname();
   return (
-    <header className="shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[80%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card/50 backdrop-blur-lg">
-      <Link href="/" className="font-bold text-lg flex items-center">
-        <Image src="/logo.png" alt="logo" width={60} height={60} />
-      </Link>
-      {/* <!-- Mobile --> */}
-      <div className="flex items-center lg:hidden">
-        <Sheet open={isOpen} onOpenChange={setIsOpen}>
-          <SheetTrigger asChild>
-            <Menu
-              onClick={() => setIsOpen(!isOpen)}
-              className="cursor-pointer lg:hidden"
-            />
-          </SheetTrigger>
+    <header className="container sticky top-5 z-40">
+      <nav className="shadow-inner bg-opacity-15  border border-secondary rounded-2xl flex justify-between items-center py-2 px-4 bg-card/50 backdrop-blur-lg">
+        <Link href="/" className="font-bold text-lg flex items-center">
+          <Image src="/logo.png" alt="logo" width={60} height={60} />
+        </Link>
+        {/* <!-- Mobile --> */}
+        <div className="flex items-center lg:hidden">
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger asChild>
+              <Menu
+                onClick={() => setIsOpen(!isOpen)}
+                className="cursor-pointer lg:hidden"
+              />
+            </SheetTrigger>
 
-          <SheetContent
-            side="left"
-            className="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card border-secondary"
-          >
-            <div>
-              <SheetHeader className="mb-4 ml-4">
-                <SheetTitle className="flex items-center">
-                  <Link href="/" className="flex items-center">
-                    <Image src="/logo.png" alt="logo" width={60} height={60} />
-                  </Link>
-                </SheetTitle>
-              </SheetHeader>
+            <SheetContent
+              side="left"
+              className="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card border-secondary"
+            >
+              <div>
+                <SheetHeader className="mb-4 ml-4">
+                  <SheetTitle className="flex items-center">
+                    <Link href="/" className="flex items-center">
+                      <Image
+                        src="/logo.png"
+                        alt="logo"
+                        width={60}
+                        height={60}
+                      />
+                    </Link>
+                  </SheetTitle>
+                </SheetHeader>
 
-              <div className="flex flex-col gap-2">
-                {routeList.map(({ href, label }) => (
-                  <Button
-                    key={href}
-                    onClick={() => setIsOpen(false)}
-                    asChild
-                    variant="ghost"
-                    className="justify-start text-base"
-                  >
-                    <Link href={href}>{label}</Link>
-                  </Button>
-                ))}
+                <div className="flex flex-col gap-2">
+                  {routeList.map(({ href, label }) => (
+                    <Button
+                      key={href}
+                      onClick={() => setIsOpen(false)}
+                      asChild
+                      variant="ghost"
+                      className="justify-start text-base"
+                    >
+                      <Link href={href}>{label}</Link>
+                    </Button>
+                  ))}
+                </div>
               </div>
-            </div>
 
-            <SheetFooter className="flex-col sm:flex-col justify-start items-start">
-              <Separator className="mb-2" />
+              <SheetFooter className="flex-col sm:flex-col justify-start items-start">
+                <Separator className="mb-2" />
 
-              <ToggleTheme />
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
-      </div>
+                <ToggleTheme />
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
 
-      {/* <!-- Desktop --> */}
-      <NavigationMenu className="hidden lg:block mx-auto">
-        <NavigationMenuList>
-          {/* <NavigationMenuItem>
+        {/* <!-- Desktop --> */}
+        <NavigationMenu className="hidden lg:block mx-auto">
+          <NavigationMenuList>
+            {/* <NavigationMenuItem>
             <NavigationMenuTrigger className="bg-card text-base">
               Features
             </NavigationMenuTrigger>
@@ -164,31 +170,32 @@ export const Navbar = () => {
             </NavigationMenuContent>
           </NavigationMenuItem> */}
 
-          <NavigationMenuItem>
-            {routeList.map(({ href, label }) => (
-              <NavigationMenuLink key={href} asChild>
-                <Link href={href} className="text-base px-2">
-                  {label}
-                </Link>
-              </NavigationMenuLink>
-            ))}
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+            <NavigationMenuItem>
+              {routeList.map(({ href, label }) => (
+                <NavigationMenuLink key={href} asChild>
+                  <Link href={href} className="text-base px-2">
+                    {label}
+                  </Link>
+                </NavigationMenuLink>
+              ))}
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
 
-      <div className="hidden lg:flex">
-        <ToggleTheme />
+        <div className="hidden lg:flex">
+          <ToggleTheme />
 
-        <Button asChild size="sm" variant="ghost" aria-label="View on GitHub">
-          <Link
-            aria-label="View on GitHub"
-            href="https://github.com/Ashpara10/acm-website"
-            target="_blank"
-          >
-            <Github className="size-5" />
-          </Link>
-        </Button>
-      </div>
+          <Button asChild size="sm" variant="ghost" aria-label="View on GitHub">
+            <Link
+              aria-label="View on GitHub"
+              href="https://github.com/Ashpara10/acm-website"
+              target="_blank"
+            >
+              <Github className="size-5" />
+            </Link>
+          </Button>
+        </div>
+      </nav>
     </header>
   );
 };
