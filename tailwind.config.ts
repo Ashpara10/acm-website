eb89501ff6f1c999c3e748bb7808e12a09544461
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-10-11 14:05:00 +0530

    Tailwind Fix 🤕

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,20 +99,26 @@ module.exports = {
     animate,
     require("@tailwindcss/typography"),
     addVariablesForColors,
-    function ({ matchUtilities, theme }) {
+    function ({
+      matchUtilities,
+      theme,
+    }: {
+      matchUtilities: (utilities: any, options?: any) => void;
+      theme: any;
+    }) {
       matchUtilities(
         {
-          "bg-grid": (value) => ({
+          "bg-grid": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          "bg-grid-small": (value) => ({
+          "bg-grid-small": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          "bg-dot": (value) => ({
+          "bg-dot": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
             )}")`,
@@ -127,7 +133,7 @@ module.exports = {
 /**
  * Adds CSS variables for colors in the theme
  */
-function addVariablesForColors({ addBase, theme }) {
+function addVariablesForColors({ addBase, theme }: { addBase: any; theme: any }) {
   const allColors = flattenColorPalette(theme("colors"));
   const newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
