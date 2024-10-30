ed0f379c457cfc5a3c0dbd909b0eb40ebe524858
Anugrah Sharma <anuash130c@gmail.com>
2024-10-31 01:55:01 +0530

    Adding Hover effect on the socials of team

--- src/components/layout/sections/CoreCouncil.tsx
+++ src/components/layout/sections/CoreCouncil.tsx
@@ -101,7 +101,7 @@ export const TeamSection = () => {
                       key={index}
                       href={url}
                       target="_blank"
-                      className="hover:opacity-80 transition-all hover:-translate-y-1"
+                      className=" hover:-translate-y-1 hover:opacity-80 transition-all"
                     >
                       {getSocialIcon(name)}
                     </Link>
